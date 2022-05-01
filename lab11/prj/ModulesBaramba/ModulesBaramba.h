de39a0e142df56f1154fb7b716f0ddbbde4678ed
vlad-ozh <vladozhekhovskyi@gmail.com>
2022-05-02 00:27:24 +0300

    add library ModulesBaramba

---
+++ lab11/prj/ModulesBaramba/ModulesBaramba.h
@@ -0,0 +1,15 @@
+#ifndef MODULESBARAMBA_H_INCLUDED
+#define MODULESBARAMBA_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "struct_type_project_1.h"
+
+using namespace std;
+
+void writeToRegistData(fstream &file, regEnrollment *regEn);
+void dataOutput(fstream &file, regEnrollment *regEn, int action);
+
+
+#endif // MODULESBARAMBA_H_INCLUDED
